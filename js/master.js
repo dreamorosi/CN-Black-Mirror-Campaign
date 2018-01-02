0dcfd8342bddb10131e3169091d88182367c2d75
Andrea Amorosi <dreamorosi@gmail.com>
2018-01-02 05:35:38 +0100

    intro prototype without jquery

--- js/master.js
+++ js/master.js
@@ -26,52 +26,102 @@ function stickyElement (element, className = 'sticky') {
   }
 }
 
-// var options = {
-//   root: document.querySelector('.intro'),
-//   rootMargin: '0px',
-//   threshold: 1.0
-// }
-//
-// function callback (entries, observer) {
-//   entries.forEach(entry => {
-//     console.log(entry)
-//     console.log('hollaaa')
-    // Each entry describes an intersection change for one observed
-    // target element:
-    //   entry.boundingClientRect
-    //   entry.intersectionRatio
-    //   entry.intersectionRect
-    //   entry.isIntersecting
-    //   entry.rootBounds
-    //   entry.target
-    //   entry.time
-//   })
-// }
+var options = {
+  root: null,
+  threshold: [0.25, 0.5, 0.75]
+}
 
-// var observer = new IntersectionObserver(callback, options)
-// var target = document.querySelector('#ok')
-// observer.observe(target)
+var intro = document.querySelector('.intro')
+var colors = [
+  "#F9F7E8",
+  "#F3E8DA",
+  "#EEDACC",
+  "#E8CCBF",
+  "#E5C4B9",
+  "#E5C4B9",
+  "#E5C4B9",
+  "#DEC2BB",
+  "#CCBBC1",
+  "#BBB5C7",
+  "#A8ADCA",
+  "#9DA8CB",
+  "#000"
+]
 
-$(window).ready(function() {
-  $('.slide')
-  // .scrollie({
-  //   scrollOffset : -50,
-  //   scrollingInView : function(elem) {
-  //     var bgColor = elem.data('background')
-  //
-  //     $('.slide').css('background-color', bgColor)
-  //     // $(elem).find('p').css('opacity', 1)
-  //   }
-  // })
+var viewer = {
+  direction: null,
+  isScrolling: false,
+  scrollId: null,
+  lastSnap: {},
+  tick: function (snap) {
+    this.direction = this.lastSnap.intersectionRatio < snap.intersectionRatio
+    this.lastSnap = snap
+    if (this.scrollId === null) {
+      this.scrollId = setTimeout(viewer.lock, 0)
+    } else {
+      clearTimeout(this.scrollId)
+      this.scrollId = setTimeout(viewer.lock, 2000)
+      this.isScrolling = true
+    }
+  },
+  snap: function (entries, observer) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return
+      }
+      viewer.tick(entry)
+      // console.log(entry.target)
+      if (viewer.direction && entry.intersectionRatio > 0 && entry.intersectionRatio < 0.5) {
+        // console.log('is entering', entry.target)
+        var d = entry.target.querySelector('p')
+        d.style.opacity = 1
+      }
 
-  // $('.slide p').scrollie({
-  //   scrollOffset : -150,
-  //   scrollingInView: function (elem) {
-  //     $(elem).css('opacity', 1)
-  //   },
-  //   scrollingToTheTop: function (elem) {
-  //     console.log('exiting', elem)
-  //     $(elem).css('opacity', 0)
-  //   }
-  // })
-})
+      if (viewer.direction && entry.intersectionRatio < 0.6 && entry.intersectionRatio > 0.4) {
+        // let c = Array.from(entry.target.parentElement.children)
+        // console.log(c.indexOf(entry.target))
+        let prevColor = entry.target.dataset.background - 1
+        console.log('going to prev', prevColor)
+        intro.style.backgroundColor = colors[prevColor]
+      }
+
+      if (!viewer.direction && entry.intersectionRatio < 1 && entry.intersectionRatio > 0.5) {
+        // console.log('is exiting', entry.target)
+        var d = entry.target.querySelector('p')
+        console.log(d)
+        d.style.opacity = 0
+      }
+
+      if (!viewer.direction && entry.intersectionRatio < 0.6 && entry.intersectionRatio > 0.4) {
+        let nextColor = entry.target.dataset.background + 1
+        console.log('going to next', nextColor)
+        intro.style.backgroundColor = colors[nextColor]
+      }
+
+      // Each entry describes an intersection change for one observed
+      // target element:
+      //   entry.boundingClientRect
+      //   entry.intersectionRatio
+      //   entry.intersectionRect
+      //   entry.isIntersecting
+      //   entry.rootBounds
+      //   entry.target
+      //   entry.time
+    })
+  },
+  lock: function () {
+    viewer.isScrolling = false
+    // console.log(viewer)
+  }
+}
+
+document.onload = function () {
+  var observer = new IntersectionObserver(viewer.snap, options)
+  var target = document.querySelector('.intro .slide:nth-child(1)')
+
+  // var observer2 = new IntersectionObserver(viewer.snap, options)
+  var target2 = document.querySelector('.intro .slide:nth-child(2)')
+
+  observer.observe(target)
+  observer.observe(target2)
+}()
